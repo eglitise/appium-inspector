42e2d51d4e2e17ed7fa9dee75294c8f9f08939a7
Edgars Eglītis <edgars.eglitis@outlook.com>
2024-04-29 17:31:15 +0300

    starting out on Vite migration

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        additionalData: '@root-entry-name: default;',
+      },
+    },
+  },
+  root: 'app/renderer',
+  plugins: [react()]
+});
