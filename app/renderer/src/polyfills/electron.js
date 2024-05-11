760e458774853f6f2d9c325907ca2a6adfabc8bb
Edgars Eglitis <edgars.eglitis@outlook.com>
2024-05-11 15:42:16 +0300

    fix translation paths for electron

--- app/renderer/src/polyfills/electron.js
+++ app/renderer/src/polyfills/electron.js
@@ -4,12 +4,11 @@ import {clipboard, ipcRenderer, shell} from 'electron';
 import settings from 'electron-settings';
 import fs from 'fs';
 import i18NextBackend from 'i18next-fs-backend';
-import path from 'path';
 import util from 'util';
 
 const i18NextBackendOptions = {
-  loadPath: path.join(__dirname, '{{lng}}/{{ns}}.json'),
-  addPath: path.join(__dirname, '{{lng}}/{{ns}}.json'),
+  loadPath: 'app/renderer/src/assets/locales/{{lng}}/{{ns}}.json',
+  addPath: 'app/renderer/src/assets/locales/{{lng}}/{{ns}}.json',
   jsonIndent: 2,
 };
 
